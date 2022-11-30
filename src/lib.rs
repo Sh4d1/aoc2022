ed889e8da3005ed906d393de83b9f8258fa49734
Patrik Cyvoct <patrik@ptrk.io>
2022-11-30 22:28:15 +0100

    AoC 2022 is here!
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(clippy::all)]
+#![feature(drain_filter)]
+#![feature(box_patterns)]
+#![feature(box_syntax)]
+
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+aoc_lib! { year = 2022 }
