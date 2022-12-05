430e7966f5e2a0e9edfbd21ea95a9f5af17e6206
Patrik Cyvoct <patrik@ptrk.io>
2022-12-05 12:05:50 +0100

    fix gen_day script
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- gen_day.sh
+++ gen_day.sh
@@ -1,44 +1,34 @@
 #!/bin/bash
 
 day=$1
-type=$2
 
-export day type
+export day
 
 if [[ ! -f "./src/day${day}.rs" ]]; then
     envsubst <<EOF > ./src/day${day}.rs
 #[aoc_generator(day$day)]
-pub fn input_generator(input: &str) -> $type {
-    // input.lines().map(|l| l.parse::<u64>().unwrap()).collect()
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input.lines().map(|l| l.parse::<usize>().unwrap()).collect()
 }
 #[aoc(day$day, part1)]
-pub fn part1(input: &$type) -> usize {
+pub fn part1(input: &[usize]) -> usize {
     0
 }
 #[aoc(day$day, part2)]
-pub fn part2(input: &$type) -> usize {
+pub fn part2(input: &[usize]) -> usize {
     0
 }
 #[cfg(test)]
 mod tests {
     use super::*;
+    const INPUT: &str = "";
     #[test]
     fn example1() {
-        assert_eq!(
-            part1(&input_generator(
-                ""
-            )),
-            0
-        )
+        assert_eq!(part1(&input_generator(INPUT)), 0)
     }
     #[test]
     fn example2() {
-        assert_eq!(
-            part2(&input_generator(
-                ""
-            )),
-            0
-        )
+        assert_eq!(part2(&input_generator(INPUT)), 0)
     }
 }
 EOF
